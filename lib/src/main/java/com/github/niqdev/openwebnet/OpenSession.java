e87315cee39da91a6c65b1f8cd42e2639d3586cd
niqdev <niqdev@gmail.com>
2015-12-04 19:15:49 +0100

    rewrite library

---
+++ lib/src/main/java/com/github/niqdev/openwebnet/OpenSession.java
@@ -0,0 +1,62 @@
+package com.github.niqdev.openwebnet;
+
+import com.github.niqdev.openwebnet.message.OpenMessage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ */
+public class OpenSession {
+
+    private final Channel channel;
+    private final OpenMessage request;
+    private List<OpenMessage> response = new ArrayList<>();
+
+    private OpenSession(Channel channel, OpenMessage request) {
+        this.channel = channel;
+        this.request = request;
+    }
+
+    public Channel getChannel() {
+        return channel;
+    }
+
+    public static OpenSession newCommand(OpenMessage request) {
+        return new OpenSession(Channel.COMMAND, request);
+    }
+
+    public OpenMessage getRequest() {
+        return request;
+    }
+
+    public List<OpenMessage> getResponse() {
+        return response;
+    }
+
+    public void addResponse(OpenMessage message) {
+        response.add(message);
+    }
+
+    /**
+     *
+     */
+    enum Channel {
+
+        COMMAND("*99*0##"),
+        EVENT("*99*1##");
+
+        private final String value;
+
+        Channel(String value) {
+            this.value = value;
+        }
+
+        public String value() {
+            return value;
+        }
+
+    }
+
+}
--- lib/src/main/java/com/github/niqdev/openwebnet/OpenWebNet.java
+++ lib/src/main/java/com/github/niqdev/openwebnet/OpenWebNet.java
@@ -1,82 +1,86 @@
-package com.github.niqdev.openwebnet;
-
-import com.github.niqdev.openwebnet.message.OpenMessage;
-import com.github.niqdev.openwebnet.protocol.OpenSession;
-import rx.Observable;
-
-import java.util.List;
-
-/*
- *  frame = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, *, #}
- *  starts with '*'
- *  ends with '##'
- *  separator among the tags '*'
- *  *tag1*tag2*tag3*...*tagN##
- *
- *  tag = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, #}
- *  tag can't have the couple '##'
- */
-public class OpenWebNet {
-
-    private final OpenGateway gateway;
-
-    private OpenWebNet(OpenGateway gateway) {
-        this.gateway = gateway;
-    }
-
-    public static OpenWebNet newClient(OpenGateway gateway) {
-        return new OpenWebNet(gateway);
-    }
-
-    public Observable<OpenSession> send(OpenMessage request) {
-        throw new UnsupportedOperationException("not implemented yet");
-    }
-
-    public Observable<List<OpenSession>> send(List<OpenMessage> request) {
-        throw new UnsupportedOperationException("not implemented yet");
-    }
-
-    public void listen() {
-        throw new UnsupportedOperationException("not implemented yet");
-    }
-
-    /**
-     * TODO documentation
-     */
-    public interface OpenGateway {
-
-        int DEFAULT_PORT = 2000;
-
-        /**
-         *
-         */
-        String getHost();
-
-        /**
-         *
-         */
-        int getPort();
-    }
-
-    public static OpenGateway gateway(String host, int port) {
-        // TODO validate ip
-        // TODO validate port
-        return new OpenGateway() {
-
-            @Override
-            public String getHost() {
-                return host;
-            }
-
-            @Override
-            public int getPort() {
-                return port;
-            }
-        };
-    }
-
-    public static OpenGateway defaultGateway(String host) {
-        return gateway(host, OpenGateway.DEFAULT_PORT);
-    }
-
-}
+//package com.github.niqdev.openwebnet;
+//
+//import com.github.niqdev.openwebnet.message.OpenMessage;
+//import rx.Observable;
+//
+///*
+// *  frame = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, *, #}
+// *  starts with '*'
+// *  ends with '##'
+// *  separator among the tags '*'
+// *  *tag1*tag2*tag3*...*tagN##
+// *
+// *  tag = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, #}
+// *  tag can't have the couple '##'
+// */
+//public class OpenWebNet {
+//
+//    private final OpenGateway gateway;
+//
+//    private OpenWebNet(OpenGateway gateway) {
+//        this.gateway = gateway;
+//    }
+//
+//    public static OpenWebNet newClient(OpenGateway gateway) {
+//        return new OpenWebNet(gateway);
+//    }
+//
+//    public Observable<OpenSession> send(OpenMessage request) {
+//        OpenSession.newCommand(request);
+//        throw new UnsupportedOperationException("not implemented yet");
+//    }
+//
+//    /*
+//    public Observable<List<OpenSession>> send(OpenMessage... request) {
+//        throw new UnsupportedOperationException("not implemented yet");
+//    }
+//
+//    public Observable<List<OpenSession>> send(List<OpenMessage> request) {
+//        throw new UnsupportedOperationException("not implemented yet");
+//    }
+//
+//    public void listen() {
+//        throw new UnsupportedOperationException("not implemented yet");
+//    }
+//    */
+//
+//    /**
+//     * TODO documentation
+//     */
+//    public interface OpenGateway {
+//
+//        int DEFAULT_PORT = 2000;
+//
+//        /**
+//         *
+//         */
+//        String getHost();
+//
+//        /**
+//         *
+//         */
+//        int getPort();
+//    }
+//
+//    public static OpenGateway gateway(String host, int port) {
+//        // TODO validate ip
+//        // TODO validate port
+//        return new OpenGateway() {
+//
+//            @Override
+//            public String getHost() {
+//                return host;
+//            }
+//
+//            @Override
+//            public int getPort() {
+//                return port;
+//            }
+//        };
+//    }
+//
+//    public static OpenGateway defaultGateway(String host) {
+//        return gateway(host, OpenGateway.DEFAULT_PORT);
+//    }
+//
+//}
