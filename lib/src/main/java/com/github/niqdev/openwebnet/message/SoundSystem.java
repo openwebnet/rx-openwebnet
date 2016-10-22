2ecb8a4ac4c829257c35d7746241fb496faf0273
niqdev <niqdev@gmail.com>
2016-10-22 20:44:53 +0100

    sounds: validate type

--- lib/src/main/java/com/github/niqdev/openwebnet/message/SoundSystem.java
+++ lib/src/main/java/com/github/niqdev/openwebnet/message/SoundSystem.java
@@ -23,11 +23,64 @@ public class SoundSystem extends BaseOpenMessage {
         STEREO_CHANNEL
     }
 
+    public enum Type {
+        // 0 amplifier general command
+        AMPLIFIER_GENERAL,
+        // #0-#9 amplifiers environment command
+        AMPLIFIER_GROUP,
+        // 01-99 amplifiers point to point command
+        AMPLIFIER_P2P,
+        // 100 source general command
+        SOURCE_GENERAL,
+        // 101-109 sources point to point command
+        SOURCE_P2P;
+
+        private static final String AMPLIFIER_GENERAL_COMMAND = "0";
+        private static final int AMPLIFIER_GROUP_MIN_COMMAND = 0;
+        private static final int AMPLIFIER_GROUP_MAX_COMMAND = 9;
+        private static final int AMPLIFIER_P2P_MIN_COMMAND = 1;
+        private static final int AMPLIFIER_P2P_MAX_COMMAND = 99;
+        private static final String SOURCE_GENERAL_COMMAND = "100";
+        private static final int SOURCE_P2P_MIN_COMMAND = 101;
+        private static final int SOURCE_P2P_MAX_COMMAND = 109;
+
+        /*
+         * 0 amplifier general command
+         * #0-#9 amplifiers environment command
+         * 01-99 amplifiers point to point command
+         * 100 source general command
+         * 101-109 sources point to point command
+         */
+        public static boolean isValid(Type type, String value) {
+            if (type == null || value == null || value.length() < 1 && value.length() > 3) {
+                return false;
+            }
+
+            switch (type) {
+                case AMPLIFIER_GENERAL:
+                    return AMPLIFIER_GENERAL_COMMAND.equals(value);
+                case AMPLIFIER_GROUP:
+                    return value.length() == 1
+                        && isInRange(AMPLIFIER_GROUP_MIN_COMMAND, AMPLIFIER_GROUP_MAX_COMMAND, checkIsInteger(value));
+                case AMPLIFIER_P2P:
+                    return value.length() == 2
+                        && isInRange(AMPLIFIER_P2P_MIN_COMMAND, AMPLIFIER_P2P_MAX_COMMAND, checkIsInteger(value));
+                case SOURCE_GENERAL:
+                    return SOURCE_GENERAL_COMMAND.equals(value);
+                case SOURCE_P2P:
+                    return value.length() == 3
+                        && isInRange(SOURCE_P2P_MIN_COMMAND, SOURCE_P2P_MAX_COMMAND, checkIsInteger(value));
+            }
+            throw new IllegalArgumentException("invalid type");
+        }
+    }
+
     private static final int ON_SOURCE_BASE_BAND = 0;
     private static final int ON_SOURCE_STEREO_CHANNEL = 3;
     private static final int OFF_SOURCE_BASE_BAND = 10;
     private static final int OFF_SOURCE_STEREO_CHANNEL = 13;
     private static final int STATUS = 5;
+    private static final String WHERE_GROUP_PREFIX = "#";
     private static final int WHO_16 = SOUND_SYSTEM_1.value();
     private static final int WHO_22 = SOUND_SYSTEM_2.value();
 
@@ -38,24 +91,24 @@ public class SoundSystem extends BaseOpenMessage {
     /**
      * TODO
      */
-    public static SoundSystem requestTurnOn(String where, Source source) {
-        return buildRequest(where, source, ON_SOURCE_BASE_BAND, ON_SOURCE_STEREO_CHANNEL);
+    public static SoundSystem requestTurnOn(String where, Type type, Source source) {
+        return buildRequest(where, type, source, ON_SOURCE_BASE_BAND, ON_SOURCE_STEREO_CHANNEL);
     }
 
     /**
      * TODO
      */
-    public static SoundSystem requestTurnOff(String where, Source source) {
-        return buildRequest(where, source, OFF_SOURCE_BASE_BAND, OFF_SOURCE_STEREO_CHANNEL);
+    public static SoundSystem requestTurnOff(String where, Type type, Source source) {
+        return buildRequest(where, type, source, OFF_SOURCE_BASE_BAND, OFF_SOURCE_STEREO_CHANNEL);
     }
 
-    private static SoundSystem buildRequest(String where, Source source, int baseBandValue, int stereoValue) {
-        checkAllowedWhere(where);
+    private static SoundSystem buildRequest(String where, Type type, Source source, int baseBandValue, int stereoValue) {
+        checkArgument(Type.isValid(type, where), "invalid where|type");
         switch (source) {
             case BASE_BAND:
-                return new SoundSystem(format(FORMAT_REQUEST, WHO_16, baseBandValue, where));
+                return new SoundSystem(format(FORMAT_REQUEST, WHO_16, baseBandValue, buildWhereValue(where, type)));
             case STEREO_CHANNEL:
-                return new SoundSystem(format(FORMAT_REQUEST, WHO_16, stereoValue, where));
+                return new SoundSystem(format(FORMAT_REQUEST, WHO_16, stereoValue, buildWhereValue(where, type)));
         }
         throw new IllegalArgumentException("invalid source");
     }
@@ -70,9 +123,9 @@ public class SoundSystem extends BaseOpenMessage {
     /**
      * TODO
      */
-    public static SoundSystem requestStatus(String where) {
-        checkAllowedWhere(where);
-        return new SoundSystem(format(FORMAT_DIMENSION, WHO_16, where, STATUS));
+    public static SoundSystem requestStatus(String where, Type type) {
+        checkArgument(Type.isValid(type, where), "invalid where|type");
+        return new SoundSystem(format(FORMAT_DIMENSION, WHO_16, buildWhereValue(where, type), STATUS));
     }
 
     /**
@@ -95,39 +148,6 @@ public class SoundSystem extends BaseOpenMessage {
     }
 
     /*
-     * 0 amplifier general command
-     * #0-#9 amplifiers environment command
-     * 01-99 amplifiers point to point command
-     * 100 source general command
-     * 101-109 sources point to point command
-     */
-    private static void checkAllowedWhere(String value) {
-        final int AMPLIFIER_GENERAL_COMMAND = 0;
-        final int AMPLIFIER_MIN_ENVIRONMENT_COMMAND = 0;
-        final int AMPLIFIER_MAX_ENVIRONMENT_COMMAND = 9;
-        final int AMPLIFIER_MIN_P2P_COMMAND = 1;
-        final int AMPLIFIER_MAX_P2P_COMMAND = 99;
-        final int SOURCE_GENERAL_COMMAND = 100;
-        final int SOURCE_MIN_P2P_COMMAND = 101;
-        final int SOURCE_MAX_P2P_COMMAND = 109;
-
-        checkNotNull(value, "invalid null value");
-        checkArgument(value.length() <= 3, "invalid length");
-        if (value.startsWith("#")) {
-            checkArgument(value.length() == 2, "invalid length");
-            checkRange(AMPLIFIER_MIN_ENVIRONMENT_COMMAND, AMPLIFIER_MAX_ENVIRONMENT_COMMAND, checkIsInteger(value.substring(1)));
-        } else {
-            int where = checkIsInteger(value);
-            checkArgument(
-                where == AMPLIFIER_GENERAL_COMMAND
-                || isInRange(AMPLIFIER_MIN_P2P_COMMAND, AMPLIFIER_MAX_P2P_COMMAND, where)
-                || where == SOURCE_GENERAL_COMMAND
-                || isInRange(SOURCE_MIN_P2P_COMMAND, SOURCE_MAX_P2P_COMMAND, where),
-                "invalid where range");
-        }
-    }
-
-    /*
      * Verify OpenWebNet message response if amplifier/source is on.
      *
      * From documentation:
@@ -145,9 +165,22 @@ public class SoundSystem extends BaseOpenMessage {
         List<OpenMessage> validResponses = FluentIterable.from(responses)
             .filter(response ->
                 (response.getValue().startsWith(responseOnBaseBand) || response.getValue().startsWith(responseOnStereo))
-                && !request.getValue().equals(requestStatus("0").getValue())
+                && !request.getValue().equals(requestStatus("0", Type.AMPLIFIER_GENERAL).getValue())
             ).toList();
         return validResponses.size() >= 1;
+    }
+
+    private static String buildWhereValue(String where, Type type) {
+        switch (type) {
+            case AMPLIFIER_GENERAL:
+            case AMPLIFIER_P2P:
+            case SOURCE_GENERAL:
+            case SOURCE_P2P:
+                return where;
+            case AMPLIFIER_GROUP:
+                return WHERE_GROUP_PREFIX.concat(where);
+        }
+        throw new IllegalArgumentException("invalid type");
     }
 
 }
